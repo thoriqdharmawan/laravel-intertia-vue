3c6bf710e9318b4f4af6a89f6de5261937ea0f4c
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-14 16:54:42 +0700

    feat: implement active style state

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -7,15 +7,22 @@
     <header>
       <nav>
         <div class="space-x-6">
-          <Link :href="route('home')" class="nav-link">Home</Link>
+          <Link :href="route('home')" class="nav-link" :class="{ 'bg-slate-700': $page.component === 'Home' }">Home
+          </Link>
         </div>
         <div v-if="$page.props.auth.user" class="space-x-6">
-          <Link :href="route('dashboard')" class="nav-link">Dashboard</Link>
+          <Link :href="route('dashboard')" class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Dashboard' }">
+          Dashboard</Link>
           <Link :href="route('logout')" method="post" as="button" type="button" class="nav-link">Logout</Link>
         </div>
         <div v-else class="space-x-6">
-          <Link :href="route('register')" class="nav-link">Register</Link>
-          <Link :href="route('login')" class="nav-link">login</Link>
+          <Link :href="route('register')" class="nav-link"
+            :class="{ 'bg-slate-700': $page.component === 'Auth/Register' }">
+          Register</Link>
+          <Link :href="route('login')" class="nav-link" :class="{ 'bg-slate-700': $page.component === 'Auth/Login' }">
+          login
+          </Link>
         </div>
       </nav>
     </header>

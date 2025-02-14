ea64d85fbd8a825a14eacea7cdc3bf4e7e513795
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-14 16:57:20 +0700

    feat: add auth-guest middlewares

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Home')->name("home");
-Route::inertia('/register', 'Auth/Register')->name("register");
-Route::post('/register', [AuthController::class, 'register']);
 
-Route::inertia('/login', 'Auth/Login')->name("login");
-Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth')->group(function () {
+  Route::post('/logout', [AuthController::class, 'logout'])->name("logout");
+  Route::inertia('/dashboard', 'Dashboard')->name("dashboard");
+});
 
-Route::post('/logout', [AuthController::class, 'logout'])->name("logout");
-
-Route::inertia('/dashboard', 'Dashboard')->name("dashboard");
+Route::middleware('guest')->group(function () {
+  Route::inertia('/register', 'Auth/Register')->name("register");
+  Route::post('/register', [AuthController::class, 'register']);
+  Route::inertia('/login', 'Auth/Login')->name("login");
+  Route::post('/login', [AuthController::class, 'login']);
+});

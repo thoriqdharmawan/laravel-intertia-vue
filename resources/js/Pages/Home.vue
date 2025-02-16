66d86c541084c110fa1838a74a0a2421285aba00
thoriqdharmawan <thoriqd21@gmail.com>
2025-02-16 15:38:11 +0700

    feat: add a new list users

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,51 @@
 <script setup>
-import { usePage } from '@inertiajs/vue3';
-import { computed } from 'vue';
+import PaginationLinks from './Components/PaginationLinks.vue';
 
-const page = usePage()
+defineProps({
+  users: Object,
+});
 
-const user = computed(() => page.props.auth.user)
+const getDate = (date) => {
+  return new Date(date).toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
 </script>
 
 <template>
 
   <Head :title="` | ${$page.component}`"></Head>
 
-  <h1>Home page</h1>
+  <div>
+    <table>
+      <thead>
+        <tr class="bg-slate-300">
+          <th>Avatar</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Registration Date</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="user in users.data" :key="user.id">
+          <td>
+            <img :src="user.avatar
+              ? `storage/${user.avatar}`
+              : 'storage/avatars/default.png'
+              " class="avatar" />
+          </td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ getDate(user.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div>
+      <PaginationLinks :paginator="users" />
+    </div>
+  </div>
 </template>
